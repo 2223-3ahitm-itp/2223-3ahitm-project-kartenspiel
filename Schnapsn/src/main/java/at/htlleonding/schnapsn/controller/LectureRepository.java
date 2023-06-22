575d90b64e0e3bd34eca9a7e475051e53360d870
M0Wagner <moritz.wagner06@gmx.at>
2023-06-22 13:39:53 +0200

    23card-23 created LectureRepository and added method update

--- Schnapsn/src/main/java/at/htlleonding/schnapsn/controller/LectureRepository.java
+++ Schnapsn/src/main/java/at/htlleonding/schnapsn/controller/LectureRepository.java
@@ -1,2 +1,30 @@
-package at.htlleonding.schnapsn.controller;public class LectureRepository {
+package at.htlleonding.schnapsn.controller;
+
+import at.htlleonding.schnapsn.model.Lecture;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class LectureRepository {
+    private DataSource dataSource = Database.getDataSource();
+
+    public void update(Lecture lecture) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "update lecture SET name=?, content=? where lectureId=?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, lecture.getName());
+            statement.setString(2, lecture.getContent());
+            statement.setLong(3, lecture.getId());
+
+            if (statement.executeUpdate() == 0) {
+                throw new SQLException("Update of LECTURE failed, no rows affected");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
