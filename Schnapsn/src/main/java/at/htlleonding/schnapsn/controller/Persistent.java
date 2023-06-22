b92d0fdadafb0c52e2519bffcdd7344f6ec40329
MaxaxSlabschi <maximilian.slabschi@mmcx.net>
2023-06-22 11:55:13 +0200

    created interface persisitent

---
+++ Schnapsn/src/main/java/at/htlleonding/schnapsn/controller/Persistent.java
@@ -0,0 +1,15 @@
+package at.htlleonding.schnapsn.controller;
+
+import java.util.List;
+
+public interface Persistent<T> {
+    public void save(T entity);
+
+    public void insert(T entity);
+
+    public void delete(long id);
+
+    public List<T> findAll();
+
+    public T findById(long id);
+}
